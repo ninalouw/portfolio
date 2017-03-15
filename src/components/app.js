b915603aeea6565e576bf53911a21e5298d62b6b
Nina Louw <ninalouw@mac.com>
2017-03-15 10:57:07 -0700

    Add gitignore, eslint, material-ui

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <MuiThemeProvider>
+        <div>Nina Louw Resume</div>
+    </MuiThemeProvider>
+
     );
   }
 }
